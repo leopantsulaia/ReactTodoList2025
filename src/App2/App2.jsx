dc15f8d0069cae22803bf03891d12c3f98be9356
leopantsulaia <leopantsulaia@gmail.com>
2025-04-15 06:21:22 +0400

    latest bugs fixed

--- src/App2/App2.jsx
+++
@@ -1,5 +0,0 @@
-import MainApp from "./MainApp";
-
-export default function App() {
-  return <MainApp />;
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
+// import App from './App1/App1'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
